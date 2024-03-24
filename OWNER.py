e0bc63dc5def769f2c0b096e07de3abc23ba30a1
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-03-24 22:37:23 +0200

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["K_o_c_1", "K_o_c_1"]
+OWNER_NAME = "𑲯𓌹𖤍𖣩ًََِْٓ ✹⃝‌꙰🇪🇬 𝐕𝐄𝐍𝐎𝐌☬『‌مــمول』"
+BOT_TOKEN = "7111806231:AAGyf5Qjzxq_NKPVnhd3Yy8-8_1q2Hq-nIQ"
+DATABASE = "mongodb+srv://toxi:toxiiiic1234@atlascluster.dk5l1pm.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/K_o_c_3"
+GROUP = "https://t.me/q1_b1"
+VIDEO = "https://t.me/K_o_c_3/958"
+LOGS = "K_o_c_3"
+
